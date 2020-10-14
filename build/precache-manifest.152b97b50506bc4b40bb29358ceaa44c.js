ca786cd8ce9b77620514bb9b18cd665a00456533
siorikitajima <siorikitajima@sioris-imac.lan>
2020-10-14 13:14:58 -0700

    Fixing git 2

---
+++ build/precache-manifest.152b97b50506bc4b40bb29358ceaa44c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9d6d39bf6d740bf08cefde445720fa",
+    "url": "/strangers/index.html"
+  },
+  {
+    "revision": "3ec6325a900cfe4de7bd",
+    "url": "/strangers/static/css/main.ca98b27d.chunk.css"
+  },
+  {
+    "revision": "5a127247210a7c6b943f",
+    "url": "/strangers/static/js/2.82849539.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/strangers/static/js/2.82849539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec6325a900cfe4de7bd",
+    "url": "/strangers/static/js/main.414ea44b.chunk.js"
+  },
+  {
+    "revision": "f65ac0104e05f8b69b70",
+    "url": "/strangers/static/js/runtime-main.89638512.js"
+  },
+  {
+    "revision": "57d00ac06aeb320736d67923cc209c94",
+    "url": "/strangers/static/media/CC-BY-SA_icons.57d00ac0.svg"
+  },
+  {
+    "revision": "a0754566df0f44d603234fea8d8535b9",
+    "url": "/strangers/static/media/Pause_btn.a0754566.svg"
+  },
+  {
+    "revision": "78cf1435bc17f912c782df04f784280e",
+    "url": "/strangers/static/media/Send_btn.78cf1435.svg"
+  },
+  {
+    "revision": "598f7af9aa047f8575a4c4e4b37c3a0e",
+    "url": "/strangers/static/media/aGirl.598f7af9.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/strangers/static/media/github-icon.76d344f0.svg"
+  },
+  {
+    "revision": "d4ead4bdf859b094b46a318a9de2eda7",
+    "url": "/strangers/static/media/leftTriangle.d4ead4bd.svg"
+  },
+  {
+    "revision": "36454b676329fe1b36f2219e8a2e49e5",
+    "url": "/strangers/static/media/menu.36454b67.svg"
+  },
+  {
+    "revision": "994e5855ffa70477309dc4b253cc550a",
+    "url": "/strangers/static/media/opening-title.994e5855.jpg"
+  },
+  {
+    "revision": "7d94b185a2b2a4b131b213f681f23164",
+    "url": "/strangers/static/media/quoteClose.7d94b185.svg"
+  },
+  {
+    "revision": "5c02ef376f0dff063c768a3977c24b80",
+    "url": "/strangers/static/media/quoteOpen.5c02ef37.svg"
+  },
+  {
+    "revision": "e26c63ce3169cefd41dcf319fedc5875",
+    "url": "/strangers/static/media/rightTriangle2.e26c63ce.svg"
+  },
+  {
+    "revision": "747394e6641b37ef11a0114041744eb5",
+    "url": "/strangers/static/media/video-temp.747394e6.jpg"
+  }
+]);
